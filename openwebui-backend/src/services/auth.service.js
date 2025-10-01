301fbec0288491fe67a0d666abb296206cec7649
dev-am-tussle <ashutoshm@tussledigital.com>
2025-10-01 13:36:38 +0530

    refactor: replace bcrypt with bcryptjs for password hashing
    - Updated package.json and package-lock.json to replace bcrypt with bcryptjs.
    - Modified auth.service.js to import bcrypt from bcryptjs instead of bcrypt.

--- openwebui-backend/src/services/auth.service.js
+++ openwebui-backend/src/services/auth.service.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User, createUserWithDefaults } from "../models/user.models.js";
 import { Conversation } from "../models/conversation.model.js";
